51bb96197cf1faddcd418b6b4b3e21dc0ee34f84
Vu Ho <vuho23@gmail.com>
2020-08-29 20:03:27 +0700

    first commit

---
+++ Homework day1.js
@@ -0,0 +1,29 @@
+const message = 'Coding is great'
+const studentCount = 0;
+
+
+
+
+console.log(message);
+console.log(studentCount)
+const message1 ='coding might not be easy, but still great'
+let studentCount1 = 16
+
+console.log(message1)
+console.log(studentCount1)
+console.log(studentCount1 + 1)
+alert('Hello')
+const hobby= prompt('Your hobby is?');
+console.log(hobby)
+const firstName= prompt('your first name is?')
+const lastName= prompt('your last name is?')
+console.log(firstName)
+console.log(lastName )
+alert('HI'+  firstName +  lastName)
+const a= prompt('what is the length of the square')
+alert("The area of this square is" + a*a)
+
+var b=prompt("Celcious degree")
+console.log(b)
+alert("F degree is"+b*1.8+32)
+
